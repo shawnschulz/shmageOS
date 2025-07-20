ab177e41a2c498defcf21ad2d7ffb6d197cb17ff
shawnschulz <shawnschulz@gberkeley.edu>
2025-07-19 22:44:39 -0700

    Add initial files

---
+++ src/main.rs
@@ -0,0 +1,29 @@
+#![no_std]
+#![no_main]
+
+use core::panic::PanicInfo;
+
+/// This function is called on panic.
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+
+static HELLO: &[u8] = b"Welcome to shmageOS!";
+
+// This removes name mangling so compiler
+// doesn't try to link to C std library
+#[unsafe(no_mangle)]
+pub extern "C" fn _start() -> ! {
+
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
+
+    loop {}
+}
