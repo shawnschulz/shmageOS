87d8e628b2c537cae0bb3e0882b4f845c9b967e4
shawnschulz <shawnschulz@gberkeley.edu>
2025-08-02 23:11:14 -0700

    Add ability to check page table entries

--- src/lib.rs
+++ src/lib.rs
@@ -24,6 +24,11 @@ pub fn init() {
     unsafe { interrupts::PICS.lock().initialize() };
     x86_64::instructions::interrupts::enable();
 }
+pub fn hlt_loop() -> ! {
+    loop {
+        x86_64::instructions::hlt();
+    }
+}
 
 
 pub fn shfetch() {
--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,7 @@ mod serial;
 use core::panic::PanicInfo;
 use shos::println;
 use x86_64::registers::control::Cr3Flags;
+use::bootloader_api::{BootInfo, entry_point};
 
 /// This function is called on panic.
 #[cfg(not(test))] // new attribute
@@ -20,30 +21,36 @@ fn panic(info: &PanicInfo) -> ! {
     }
 }
 
-#[cfg(test)]
-#[panic_handler]
-fn panic(info: &PanicInfo) -> ! {
-    shos::test_panic_handler(info)
-}
-
-// This removes name mangling so compiler
-// doesn't try to link to C std library
-#[unsafe(no_mangle)]
-pub extern "C" fn _start() -> ! {
+fn kernel_main (boot_info: &'static mut BootInfo) -> ! {
+    use shos::memory::active_level_4_table;
+    use x86_64::VirtAddr;
     shos::init();
-
     shos::shfetch();
-    use x86_64::registers::control::Cr3;
-    let (phys_frame, flags) = Cr3::read();
-    println!("Level 4 page table at: {:?}", phys_frame.start_address());
-    println!("Flags: {:?}", flags);
+    let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset.into_option().unwrap());
+    let l4_table = unsafe { active_level_4_table(phys_mem_offset) };
+
+    for (i, entry) in l4_table.iter().enumerate() {
+        if !entry.is_unused() {
+            println!("L4 entry: {}: {:?}", i, entry);
+            // get physical address from entry and convert
+            let physal_address = entry.frame().unwrap().start_address();
+            let virtual_address = physal_address.as_u64() + boot_info.physical_memory_offset.into_option().unwrap();
+            let mut virtual_ptr = VirtAddr::new(virtual_address).as_mut_ptr();
+        }
+    }
+    use shos::print;
     // invoke a breakpoint exception
     // You can also panic at any point!
     //panic!("uh oh!");
     #[cfg(test)]
     test_main();
-    use shos::print;
-    loop{
-        x86_64::instructions::hlt();
-    }
+    shos::hlt_loop();
+}
+
+entry_point!(kernel_main);
+
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    shos::test_panic_handler(info)
 }
